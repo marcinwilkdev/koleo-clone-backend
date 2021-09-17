243ebf3214cf9266b399386f305c35c33823776e
Marcin Wilk <marolis1239@gmail.com>
2021-09-17 21:02:14 +0200

    connect app to mongodb database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-    res.send("HELLO WORLD!");
-});
+if(process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 
-app.listen(8080);
+mongoose
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hew9q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+    .then(() =>
+        app.listen(8080, () =>
+            console.log("SERVER STARTED ON http://localhost:8080")
+        )
+    )
+    .catch((err) => console.log(err));
