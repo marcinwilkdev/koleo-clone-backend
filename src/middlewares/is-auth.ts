e14cc8ff527450ac3fe9702da86ed6d173d6d45a
Marcin Wilk <marolis1239@gmail.com>
2021-09-18 00:00:55 +0200

    create isAuth middleware (not tested)

---
+++ src/middlewares/is-auth.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import HttpException from "../util/HttpException";
+
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const isAuth = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.get("Authorization");
+
+    if(!authHeader) {
+        throw new HttpException("Authorization failed.", 401);
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if(!token) {
+        throw new HttpException("Authorization failed.", 401);
+    }
+
+    if(!process.env.JWT_SECRET) {
+        throw new Error();
+    }
+
+    const jwtPayload = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+
+    if(!jwtPayload) {
+        throw new HttpException("Authorization failed.", 401);
+    }
+
+    req.userId = jwtPayload.userId;
+
+    next();
+}; // NOT TESTED
