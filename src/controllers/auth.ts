a4a5b19f28e6e33f6d0762eb756576ef61f05017
Marcin Wilk <marolis1239@gmail.com>
2021-09-17 21:38:09 +0200

    add signup validation

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,9 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { handleErrors, validateRequest } from "../util/helpers";
 
-export const signin = (req: Request, res: Response, next: NextFunction) => {
+export const signin = (req: Request, res: Response, next: NextFunction) => {};
 
-};
-
-export const signup = (req: Request, res: Response, next: NextFunction) => {
-    
+export const signup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        validateRequest(req);
+        res.status(200).json({ message: "SUCCESS" });
+    } catch (err) {
+        handleErrors(err, next);
+    }
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,9 +3,36 @@ import { signin, signup } from "../controllers/auth";
 
 import { body } from "express-validator";
 
+import User from "../models/user";
+
 const router = Router();
 
-router.put("/signup", signup);
+router.put(
+    "/signup",
+    [
+        body("email")
+            .isEmail()
+            .custom(async (value, { req }) => {
+                const user = await User.findOne({ email: value });
+
+                if(user) {
+                    throw Error("User with this e-mail address already exists.");
+                }
+
+                return true;
+            })
+            .normalizeEmail(),
+        body("password").trim().isLength({min: 8}),
+        body("confirmPassword").trim().custom((value, {req}) => {
+            if(value !== req.body.password) {
+                throw new Error("Passwords don't match.");
+            }
+
+            return true;
+        })
+    ],
+    signup
+);
 router.post("/signin", signin);
 
 export default router;
