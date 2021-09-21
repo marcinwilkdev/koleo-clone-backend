e970ed7177debd9c7889cc5153a07ea711970bf4
Marcin Wilk <marolis1239@gmail.com>
2021-09-21 11:51:29 +0200

    create interface for error response body

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,11 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import HttpException from "../util/HttpException";
 
+interface ErrorResponseBody {
+    errorMessage: string;
+}
+
 export const errorsHandler = (
     err: HttpException,
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    res.status(err.statusCode).json({ message: err.message });
+    const statusCode = err.statusCode;
+
+    const responseBody: ErrorResponseBody = {
+        errorMessage: err.message,
+    };
+
+    res.status(statusCode).json(responseBody);
 };
