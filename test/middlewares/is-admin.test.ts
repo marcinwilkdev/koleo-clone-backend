973a690b8180e2ce7e783c0debef687c6cc7520c
Marcin Wilk <marolis1239@gmail.com>
2021-09-24 14:00:15 +0200

    create isAdmin tests

---
+++ test/middlewares/is-admin.test.ts
@@ -0,0 +1,81 @@
+import { NextFunction, Request } from "express";
+import { expect } from "chai";
+
+import { isAdmin } from "../../src/middlewares/is-admin";
+
+import { createResponse } from "../createResponse";
+import HttpException from "../../src/util/HttpException";
+import UserService from "../../src/services/database/UserService";
+import { ISavedUser } from "../../src/models/user";
+
+const savedUser: ISavedUser = {
+    id: "",
+    email: "",
+    password: "",
+    dateOfBirth: new Date(),
+    discount: "",
+    firstName: "",
+    lastName: "",
+};
+
+const req = {
+    userId: ""
+} as unknown as Request;
+
+describe("isAdmin middleware", () => {
+    const res = createResponse();
+
+    let exception: null | HttpException;
+    let nextCalled = false;
+
+    const next = ((exc: HttpException) => {
+        exception = exc;
+        nextCalled = true;
+    }) as unknown as NextFunction;
+
+    before(() => {
+        UserService.init({
+            save: async () => savedUser,
+            update: async () => savedUser,
+            findByEmail: async () => null,
+            findById: async () => null,
+        });
+    });
+
+    it("should throw 'User not found.' if user was not found", (done) => {
+        isAdmin(req, res, next).then(() => {
+            expect(exception).to.have.property(
+                "message", "User not found."
+            );
+
+            done();
+        })
+    });
+
+    it("should throw 'User is not an admin.' if user is not an admin", (done) => {
+        UserService.getInstance().findById = async () => savedUser;
+
+        isAdmin(req, res, next).then(() => {
+            expect(exception).to.have.property(
+                "message", "User is not an admin."
+            );
+
+            done();
+        })
+    });
+
+    it("should call next without exception if user is an admin", (done) => {
+        savedUser.isAdmin = true;
+
+        nextCalled = false;
+
+        isAdmin(req, res, next).then(() => {
+            console.log(nextCalled, exception);
+
+            expect(exception).to.be.equal(undefined);
+            expect(nextCalled).to.be.equal(true);
+
+            done();
+        })
+    });
+});
