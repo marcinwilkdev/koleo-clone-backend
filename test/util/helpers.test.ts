4fae5bd074795f5d8fb005655f63f29d5b822fe4
Marcin Wilk <marolis1239@gmail.com>
2021-09-24 13:51:06 +0200

    create getUserDiscount tests

--- test/util/helpers.test.ts
+++ test/util/helpers.test.ts
@@ -1,12 +1,32 @@
-import { NextFunction } from "express";
+import { NextFunction, Request } from "express";
 
 import { expect } from "chai";
 
-import { handleErrors } from "../../src/util/helpers";
+import { getUserDiscount, handleErrors } from "../../src/util/helpers";
 import HttpException from "../../src/util/HttpException";
+import UserService from "../../src/services/database/UserService";
+import { ISavedUser } from "../../src/models/user";
+
+const savedUser: ISavedUser = {
+    id: "",
+    email: "",
+    password: "",
+    dateOfBirth: new Date(),
+    discount: "true",
+    firstName: "",
+    lastName: "",
+};
+
+const req = {
+    userId: "xyz"
+} as unknown as Request;
 
 describe("handle errors", () => {
     let exception: HttpException | null;
+
+    const next = (exc: HttpException) => {
+        exception = exc;
+    };
 
     beforeEach(() => {
         exception = null;
@@ -14,10 +34,6 @@ describe("handle errors", () => {
 
     it("should send 'Something went wrong.' 500 http exception if no err passed", () => {
         const err = {};
-
-        const next = (exc: HttpException) => {
-            exception = exc;
-        };
 
         handleErrors(err, next as unknown as NextFunction);
 
@@ -30,10 +46,6 @@ describe("handle errors", () => {
             message: "Test message",
             statusCode: 400,
         };
-        
-        const next = (exc: HttpException) => {
-            exception = exc;
-        };
 
         handleErrors(err, next as unknown as NextFunction);
 
@@ -41,3 +53,32 @@ describe("handle errors", () => {
         expect(exception).to.have.property("statusCode", 400);
     });
 });
+
+describe("get userDiscount", () => {
+    before(() => {
+        UserService.init({
+            findByEmail: async () => null,
+            findById: async () => null,
+            save: async () => savedUser,
+            update: async () => savedUser
+        })
+    })
+
+    it("should return 0 if user doesn't have discount or user not present", (done) => {
+        getUserDiscount(req).then((discount) => {
+            expect(discount).to.be.equal(0);
+
+            done();
+        })
+    })
+
+    it("should return 0.5 if user has discount", (done) => {
+        UserService.getInstance().findById = async () => savedUser;
+
+        getUserDiscount(req).then((discount) => {
+            expect(discount).to.be.equal(0.5);
+
+            done();
+        })
+    })
+})
