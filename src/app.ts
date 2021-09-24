b29015521ba4625e2eea58e9ecafe4eed6f64e93
Marcin Wilk <marolis1239@gmail.com>
2021-09-24 15:17:17 +0200

    change port to env

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,7 @@ mongoose
         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hew9q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     )
     .then(() =>
-        app.listen(8080, () =>
+        app.listen(process.env.PORT || 8080, () =>
             console.log("SERVER STARTED")
         )
     )
