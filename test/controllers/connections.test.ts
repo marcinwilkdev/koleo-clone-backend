6eee1aae04a79551265ed17b73c1a61b073d15c9
Marcin Wilk <marolis1239@gmail.com>
2021-09-24 13:42:06 +0200

    finish creating connections tests

--- test/controllers/connections.test.ts
+++ test/controllers/connections.test.ts
@@ -3,9 +3,25 @@ import { createResponse } from "../createResponse";
 import ConnectionService from "../../src/services/database/ConnectionService";
 import { ISavedConnection } from "../../src/models/connection";
 
-import { addConnection } from "../../src/controllers/connections";
-import { Request } from "express";
+import {
+    addConnection,
+    findConnections,
+} from "../../src/controllers/connections";
+import { NextFunction, Request } from "express";
 import { expect } from "chai";
+import HttpException from "../../src/util/HttpException";
+import UserService from "../../src/services/database/UserService";
+import { ISavedUser } from "../../src/models/user";
+
+const savedUser: ISavedUser = {
+    id: "",
+    email: "",
+    password: "",
+    dateOfBirth: new Date(),
+    discount: "",
+    firstName: "",
+    lastName: "",
+};
 
 const savedConnection: ISavedConnection = {
     cities: [],
@@ -16,8 +32,9 @@ const savedConnection: ISavedConnection = {
 const req = {
     body: {
         cities: [],
-        trainType: ""
-    }
+        trainType: "",
+    },
+    query: {}
 } as unknown as Request;
 
 describe("connections controller - add connection", () => {
@@ -31,7 +48,7 @@ describe("connections controller - add connection", () => {
         });
     });
 
-    it("should send correct response if user has been created", (done) => {
+    it("should send correct response if connection has been created", (done) => {
         addConnection(req, res, () => {}).then(() => {
             expect(res).to.have.property("statusCode", 201);
             expect(res.body).to.have.property(
@@ -44,4 +61,54 @@ describe("connections controller - add connection", () => {
     });
 });
 
-describe("connections controller - find connections", () => {});
+describe("connections controller - find connections", () => {
+    const res = createResponse();
+
+    let exception: null | HttpException;
+
+    const next = ((exc: HttpException) => {
+        exception = exc;
+    }) as unknown as NextFunction;
+
+    before(() => {
+        ConnectionService.init({
+            findById: async () => null,
+            getConnectionsByCities: async () => [],
+            save: async () => savedConnection,
+        });
+
+        UserService.init({
+            save: async () => savedUser,
+            update: async () => savedUser,
+            findByEmail: async () => null,
+            findById: async () => null,
+        });
+    });
+
+    it("should throw 'Couldn't find connections.' if query parameters not present", (done) => {
+        findConnections(req, res, next).then(() => {
+            expect(exception).to.have.property(
+                "message",
+                "Couldn't find connections."
+            );
+
+            done();
+        });
+    });
+
+    it("should send correct response if connections have been fetched", (done) => {
+        req.query.from = "xyz";
+        req.query.to = "xyz";
+        
+        
+        findConnections(req, res, next).then(() => {
+            expect(res).to.have.property("statusCode", 200);
+            expect(res.body).to.have.property(
+                "message",
+                "Connections fetched succesfully."
+            );
+
+            done();
+        });
+    })
+});
